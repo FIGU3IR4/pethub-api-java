ee4820069425e074d09d5cd6c26a90281eb176f0
Victor Figueira <168022769+FIGU3IR4@users.noreply.github.com>
2026-03-09 12:16:53 -0300

    primeiro commit

---
+++ src/test/java/com/example/pethub/PethubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pethub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PethubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
